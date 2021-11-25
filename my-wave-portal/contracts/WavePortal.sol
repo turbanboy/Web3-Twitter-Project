495be948586343f2c5f209610f85927359a1730e
Harsimran Singh <hsinghk7@gmail.com>
2021-11-25 12:41:39 -0800

    Cleaner code

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -18,32 +18,22 @@ contract WavePortal {
 
     Wave[] waves;
 
-    /*
-     * This is an address => uint mapping, meaning I can associate an address with a number!
-     * In this case, I'll be storing the address with the last time the user waved at us.
-     */
     mapping(address => uint256) public lastWavedAt;
 
     constructor() payable {
         console.log("We have been constructed!");
-        /*
-         * Set the initial seed
-         */
+ 
         seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
-        /*
-         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
-         */
+
         require(
             lastWavedAt[msg.sender] + 15 seconds < block.timestamp,
             "Wait 15m"
         );
 
-        /*
-         * Update the current timestamp we have for the user
-         */
+     
         lastWavedAt[msg.sender] = block.timestamp;
 
         totalWaves += 1;
@@ -51,9 +41,6 @@ contract WavePortal {
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
-        /*
-         * Generate a new seed for the next user that sends a wave
-         */
         seed = (block.difficulty + block.timestamp + seed) % 100;
 
         if (seed <= 50) {
